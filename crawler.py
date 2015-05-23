6f03fa6a32538c48834add0490c7d2f2256653dd
Wonjae Kim <fkdlvm1109@gmail.com>
2015-05-24 00:10:42 +0900

    refactor beautifulsoup -> lxml
    Crawl was very fast.

--- crawler.py
+++ crawler.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup as bs
-from compiler.ast import flatten
-import urllib, json, re
+from lxml import html
+import requests, json, re, itertools
 
 naverComicUrl = 'http://comic.naver.com'
 naverComicListUrl = naverComicUrl + '/webtoon/creation.nhn?view=image'
 
 
 def init_thumbnails():
-	naverComicData = urllib.urlopen(naverComicListUrl).read()
-	naverComicSoup = bs(naverComicData)
-	thumbnailDivisions = naverComicSoup.find_all('div', 'thumb')
-	thumbnails = [{'title': td.a['title'], 'href': td.a['href'], 'imgsrc': td.a.img['src'], 'id': re.search('[0-9]+', td.a['href']).group(0)} for td in thumbnailDivisions]
+	response = requests.get(naverComicListUrl)
+	parsedBody = html.fromstring(response.text)
+	thumbnailDivisions = parsedBody.xpath("//div[@class='thumb']")
+	thumbnails = [{'title': td.xpath('./a/@title')[0], 'href': td.xpath('./a/@href')[0], 'imgsrc': td.xpath('./a/img/@src')[0], 'id': re.search('[0-9]+', td.xpath('./a/@href')[0]).group(0)} for td in thumbnailDivisions]
 
 	for thumbnail in thumbnails:
 		print 'Getting Stars from ' + thumbnail['title'].encode('utf-8')
@@ -23,15 +22,22 @@ def init_thumbnails():
 		json.dump(thumbnails, fp)
 
 def endPageNum(href):
-	return int(bs(urllib.urlopen(naverComicUrl + href + '&page=1000').read()).find_all('span', 'current')[0].string)
+	return int(html.fromstring(requests.get(naverComicUrl + href + '&page=1000').text).xpath("//span[@class='current']")[0].text)
 
 def makeStarList(href):
 	pages = [naverComicUrl + href + '&page=' + str(num) for num in xrange(1, endPageNum(href) + 1)]
-	stars = [[float(star.string) for star in [x.find_all('strong')[0] for x in bs(urllib.urlopen(page).read()).find_all('div', 'rating_type')]] for page in pages]
-	return flatten(stars)[::-1]
+	stars = list(itertools.chain(*[[float(star.text)
+				for star
+				in [x.xpath('./strong')[0]
+					for x
+					in html.fromstring(requests.get(page).text).xpath("//div[@class='rating_type']")]]
+			for page
+			in pages]))
+	return stars[::-1]
 
 def getAuthor(href):
-	return bs(urllib.urlopen(naverComicUrl + href).read()).find_all('span', 'wrt_nm')[0].string
+	author = html.fromstring(requests.get(naverComicUrl + href).text).xpath("//span[@class='wrt_nm']")[0].text
+	return author
 
 
 if __name__ == '__main__':
