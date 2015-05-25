2eecf848167e50b97698c9d3a1614c63e32d400a
Wonjae Kim <fkdlvm1109@gmail.com>
2015-05-25 15:56:41 +0900

    modify app.py to handle data on runtime

--- app.py
+++ app.py
@@ -6,22 +6,24 @@ import json, random
 
 app = Flask(__name__)
 
+with open('static/thumbnails.json', 'rb') as fp:
+	thumbnails = json.load(fp)
+
+titles = [thumbnail['title'] for thumbnail in thumbnails]
+thumbnails_idn = dict((t['id'], t) for t in thumbnails)
+
 @app.route('/')
 def index():
-	with open('static/thumbnails.json', 'rb') as fp:
-		thumbnails = json.load(fp)
-	titles = [thumbnail['title'] for thumbnail in thumbnails]
+	global thumbnails, titles
 	return render_template('index.html', thumbnails=thumbnails, start=random.choice(thumbnails), titles = titles)
 
 @app.route('/<idn>')
 def index_with_idn(idn):
-	with open('static/thumbnails.json', 'rb') as fp:
-		thumbnails = json.load(fp)
-	thumbnails_idn = dict((t['id'], t) for t in thumbnails)
+	global thumbnails, titles, thumbnails_idn
 	if idn in thumbnails_idn.keys():
-		return render_template('index.html', thumbnails=thumbnails, start=thumbnails_idn[idn], titles = [thumbnail['title'] for thumbnail in thumbnails])
+		return render_template('index.html', thumbnails=thumbnails, start=thumbnails_idn[idn], titles = titles)
 	else:
-		return render_template('index.html', thumbnails=thumbnails, start=random.choice(thumbnails), titles = [thumbnail['title'] for thumbnail in thumbnails])
+		return render_template('index.html', thumbnails=thumbnails, start=random.choice(thumbnails), titles = titles)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=12345)
